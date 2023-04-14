9c1001cfd080a0590b3646e457e3b5901007efc5
muXxer <git@muxxer.de>
2023-04-14 15:45:15 +0200

    Adapt to latest hive.go changes

--- tools/gendoc/main.go
+++ tools/gendoc/main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/iotaledger/hive.go/app"
 	"github.com/iotaledger/hive.go/apputils/config"
-	"github.com/iotaledger/hive.go/core/app"
 	spammerApp "github.com/iotaledger/inx-spammer/core/app"
 )
 
